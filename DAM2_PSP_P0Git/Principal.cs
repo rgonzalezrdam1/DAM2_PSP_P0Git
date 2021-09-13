75fa2b76aee98576b81d669fd8f1976f0c92a6d5
pedro velasco <pvelasczuf@educacion.navarra.es>
2021-09-13 14:21:42 +0200

    Agregar archivos de proyecto.

---
+++ DAM2_PSP_P0Git/Principal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAM2_PSP_P0Git
+{
+    class Principal
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola, soy Pedro!");
+        }
+    }
+}
+
